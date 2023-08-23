ee6280e850408333b6b352da28108822bb379755
이담비 <47710007+damdam6@users.noreply.github.com>
2023-08-23 13:44:54 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 346 ms, Memory: 28,820 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -1,5 +1,7 @@
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -7,11 +9,55 @@ import java.util.Queue;
 import java.util.StringTokenizer;
  
 public class Solution {
+	
+	public static class node{
+
+		//전부 숫자로 주어지므로 숫자로 해결 가능
+		private int parent=0;
+		private int chdL=0;
+		private int chdR=0;
+		private int cur;
+		
+		public node(int num) {
+			this.cur = num;
+		}
+		
+		public void setPar(int num) {
+			this.parent=num;
+		}
+		public void setChd(int num) {
+			if(chdL==0) {
+				this.chdL = num;
+			}else if(chdR==0) {
+				this.chdR = num;
+			}else {
+				System.out.println("오류");
+			}
+		}
+		
+		public int getPar() {
+			return this.parent;
+		}
+		public int getChdL() {
+			return chdL;
+		}
+		public int getChdR() {
+			return chdR;
+		}
+		public int getM() {
+			return cur;
+		}
+		
+		public String toString() {
+			return "부모 "+parent+" 나 "+cur+" 자식 "+chdL+" "+chdR;
+		}
+		
+	}
  
     static StringBuilder sb = new StringBuilder();
  
-    public static void main(String args[]) throws Exception {
-        //System.setIn(new FileInputStream("res/input.txt"));
+    public static void main(String args[]) throws NumberFormatException, IOException {
+       //System.setIn(new FileInputStream("res/input.txt"));
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
  
         int T = Integer.parseInt(bf.readLine());
@@ -20,16 +66,7 @@ public class Solution {
             // 첫줄에 정점의개수V 간선의개수E 공통조상찾는두놈1 2
             // 간선의 나열E
             // 부모-자식 부모-자식 / 부모2-자식2....
- 
-            // 해결방안
-            // 자식 -> 루트 노드로 탐색하는 방법체크
-            // 두 개의 경로 중 가장 먼저 공통으로 등장하는 노드 check
-            // 서브 트리의 크기도??? ㅁㅊ..
- 
-            // 서브 트리는 BFS로 해결해보기
- 
-            // 자식 -> 부모 루트 탐색하기
- 
+  
             // 일단 값부터 받기 - 첫줄
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int V = Integer.parseInt(st.nextToken());
@@ -39,21 +76,34 @@ public class Solution {
  
             // 짝수 - 부모노드
             // 홀수 - 자식 노드
+            
+            //자식값과 부모값 할당
+            node[] noArr = new node[V+1];
+            for(int i=1;i<noArr.length;i++)noArr[i]=new node(i);
             st = new StringTokenizer(bf.readLine());
-            ArrayList<Integer> parArr = new ArrayList<Integer>();
-            ArrayList<Integer> subArr = new ArrayList<Integer>();
             for (int i = 0; i < E; i++) {
-                parArr.add(Integer.parseInt(st.nextToken()));
-                subArr.add(Integer.parseInt(st.nextToken()));
+            	int idx =Integer.parseInt(st.nextToken());
+            	//노드 생성
+            	 noArr[idx].setChd(Integer.parseInt(st.nextToken()));
+            	 
+             	if(noArr[idx].getChdL()!=0) {
+            		noArr[noArr[idx].getChdL()].setPar(idx);
+            	}
+            	if(noArr[idx].getChdR()!=0) {
+            		noArr[noArr[idx].getChdR()].setPar(idx);
+            	}     
+
             }
+            
+            //for(int i=1;i<noArr.length;i++)System.out.println(noArr[i].toString());
+ 
  
             ArrayList<Integer> n1Way = new ArrayList<Integer>();
- 
             // node1의 부모노드를 찾아서 기록하기
             int n1 = node1;
             while (n1 != 1) {
                 n1Way.add(n1);
-                n1 = parArr.get(subArr.indexOf(n1));
+                n1 = noArr[n1].getPar();
             }
             n1Way.add(1);
             //System.out.println(n1Way.toString());
@@ -68,25 +118,28 @@ public class Solution {
                     parent = n2;
                     break;
                 }
-                n2 = parArr.get(subArr.indexOf(n2));
+                n2 = noArr[n2].getPar();
             }
+            //System.out.println(parent);
+//// 
+            //조상을 구했으므로 걔의 서브트리 구하기 BFS?
  
-            // 조상을 구했으므로 걔의 서브트리 구하기 BFS?
- 
-            Queue<Integer> qu = new ArrayDeque<>();
+            Queue<node> qu = new ArrayDeque<>();
             int cnt = 0;
             // 탐색 시작의 대상부터 큐에 넣기
-            qu.offer(parent);
+            qu.offer(noArr[parent]);
             int idx;
             while (!qu.isEmpty()) {
                 cnt++;
                 //cur은 현재의 부모 노드
-                int cur = qu.poll();
-                while(parArr.contains(cur)) {
-                    idx = parArr.indexOf(cur);
-                    qu.offer(subArr.get(idx));
-                    parArr.set(idx, 0);
-                }
+               node cur = qu.poll();
+                
+               if(cur.chdL!=0) {
+            	   qu.add(noArr[cur.chdL]);
+               }
+               if(cur.chdR !=0) {
+            	   qu.add(noArr[cur.chdR]);
+               }
      
             }
             sb.append("#"+test_case+" "+parent+" "+cnt+"\n");
