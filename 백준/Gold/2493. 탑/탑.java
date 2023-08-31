697cc3bb5ec0b9636845e1557b755c9056e2121e
이담비 <47710007+damdam6@users.noreply.github.com>
2023-08-31 17:22:49 +0900

    [Gold V] Title: 탑, Time: 772 ms, Memory: 129780 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,48 +1,45 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		StringBuilder sb = new StringBuilder();
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(bf.readLine());
-		Stack<Integer> stk = new Stack<>();
-		Stack<Integer> idx = new Stack<>();
+		int[] arr = new int[N];
+		int[] bb = new int[N];
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-
-		int cnt = 1;
-		stk.push(Integer.parseInt(st.nextToken()));
-		idx.push(cnt);
-		sb.append("0 ");
-		for(int i=0; i<N-1;i++) {
-			cnt++;
-			//이번 탑의 높이
-			int temp = Integer.parseInt(st.nextToken());;
-
-			while(!stk.isEmpty() && stk.peek() < temp) {
-				stk.pop();
-				idx.pop();
-			}
-
-			if(!stk.isEmpty()) {
-				sb.append(idx.peek()+" ");
-				
-			}else {
-				sb.append("0 ");
-			}
-			stk.push(temp);
-			idx.push(cnt);
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
+		// index를 넣어보자 - 스택
+		Stack<Integer> stk = new Stack<>();
+		int i = N - 1;
+		while (i >= 0) {
+			if (stk.isEmpty()) {
+				stk.push(i--);
+			} else if (arr[stk.peek()] > arr[i]) {
+				stk.push(i--);
+			} else {
+
+				while (!stk.isEmpty()&&i>=0&&arr[stk.peek()] < arr[i]) {
+					bb[stk.pop()] = i+1;
+				}
+
+			}
+		}
+		while(!stk.isEmpty()) {
+			bb[stk.pop()] = 0;
+		}
+	
+		for(int c:bb)sb.append(c+" ");
 		System.out.println(sb);
-		
 	}
 
 }
