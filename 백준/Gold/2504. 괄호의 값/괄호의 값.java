51cdcba347c2b27a5b68783e0f186d627cc5425a
이담비 <dambi626626@gmail.com>
2023-11-02 12:00:40 +0900

    [Gold V] Title: 괄호의 값, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,80 +1,54 @@
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        Stack<Character> stk = new Stack<>();
+        Stack<Integer> stkVal = new Stack<>(); // 숫자를 저장하는 스택입니다.
+        
+        String input = bf.readLine();
+        
+        boolean isBalanced = true; // 괄호의 균형을 체크합니다.
+        int tempValue = 0; // 임시로 값을 계산하기 위한 변수입니다.
 
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+        for(char ch : input.toCharArray()) {
+            switch(ch) {
+                case '(':
+                case '[':
+                    stk.push(ch);
+                    stkVal.push(tempValue); // 현재까지의 값을 스택에 넣습니다.
+                    tempValue = 0; // 임시 값 초기화
+                    break;
+                case ')':
+                    if(stk.isEmpty() || stk.pop() != '(') {
+                        isBalanced = false;
+                    } else {
+                        tempValue = (tempValue == 0 ? 1 : tempValue) * 2;
+                        tempValue += stkVal.pop(); // 이전 값과 더합니다.
+                    }
+                    break;
+                case ']':
+                    if(stk.isEmpty() || stk.pop() != '[') {
+                        isBalanced = false;
+                    } else {
+                        tempValue = (tempValue == 0 ? 1 : tempValue) * 3;
+                        tempValue += stkVal.pop(); // 이전 값과 더합니다.
+                    }
+                    break;
+            }
+            if(!isBalanced) break; // 균형이 깨졌다면 반복을 종료합니다.
+        }
 
-		char[] input = bf.readLine().toCharArray();
-//		한 쌍의 괄호로만 이루어진 ‘()’와 ‘[]’는 올바른 괄호열이다.
-//		만일 X가 올바른 괄호열이면 ‘(X)’이나 ‘[X]’도 모두 올바른 괄호열이 된다.
-//		X와 Y 모두 올바른 괄호열이라면 이들을 결합한 XY도 올바른 괄호열이 된다.
-
-		Stack<Character> stk = new Stack<>();
-		Map<Character, Character> map = new HashMap<>();
-		map.put(')', '(');
-		map.put(']', '[');
-
-		Map<Character, Integer> cal = new HashMap<>();
-		cal.put('(', 2);
-		cal.put('[', 3);
-
-		int sum = 0;
-		int i = 0;
-		int[] arr = new int[31];
-		//System.out.println(Arrays.toString(input));
-		//System.out.println(Arrays.toString(arr));
-		//System.out.println(stk.toString());
-		
-		while (i < input.length) {
-			
-			if (stk.isEmpty()) {
-				
-				stk.push(input[i++]);
-				//System.out.println("비어서 집어넣음");
-			} else if (input[i] == '(' || input[i] == '[') {
-				stk.push(input[i++]);
-				//System.out.println("여는 괄호");
-			} else {
-				//System.out.println("닫는 괄호");
-				if (stk.peek() != map.get(input[i])) {
-					sum = 0;
-					break;
-				}
-				//System.out.println("------닫는 괄호의 while");
-				while (i<input.length&&!stk.isEmpty()&&stk.peek() == map.get(input[i])) {
-					if(arr[stk.size()]==0) {
-						arr[stk.size()-1] +=cal.get(stk.pop());
-					}else {
-						arr[stk.size()-1] += arr[stk.size()]*cal.get(stk.pop());
-						arr[stk.size()+1]=0;
-					}
-					i++;
-					//System.out.println();
-					//System.out.println(Arrays.toString(arr));
-					//System.out.println(stk.toString());
-					//System.out.println();
-				}
-				//System.out.println("------------여기 while종료");
-			}
-
-			//System.out.println(Arrays.toString(arr));
-			//System.out.println(stk.toString());
-		}
-		sum = arr[0];
-
-		if (!stk.isEmpty()) {
-			sum = 0;
-		}
-		System.out.println(sum);
-	}
-
+        if(!isBalanced || !stk.isEmpty()) { // 스택이 비어있지 않거나, 균형이 깨진 경우
+            System.out.println(0);
+        } else {
+            System.out.println(tempValue);
+        }
+    }
 }
