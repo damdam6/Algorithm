41a30cc336b692e44cdd573985e1a511ba131017
이담비 <dambi626626@gmail.com>
2024-04-14 22:42:51 +0900

    [level 3] Title: 등굣길, Time: 0.79 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+ 
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int val = 1000000007;
+        
+        int[][] bd = new int[n + 1][m + 1];
+        for(int i = 0; i < puddles.length; i++) {
+           bd[puddles[i][1]][puddles[i][0]] = -1; 
+        }
+        
+        bd[1][1] = 1;
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 1; j < m + 1; j++) {
+                if(bd[i][j] == -1) continue;
+                if(bd[i - 1][j] > 0) bd[i][j] += bd[i - 1][j] % val;
+                if(bd[i][j - 1] > 0) bd[i][j] += bd[i][j - 1] % val;
+            }
+        }
+        return bd[n][m] % val;
+    }
+}
