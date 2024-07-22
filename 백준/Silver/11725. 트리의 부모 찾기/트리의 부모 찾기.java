91d2f9a1dee49a3d72e2c86efd6c61e1a5723566
이담비 <dambi626626@gmail.com>
2024-07-22 20:03:43 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1396 ms, Memory: 69172 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,54 +1,53 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		StringBuilder sb = new StringBuilder();
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		//노드 갯수
-		int N = Integer.parseInt(bf.readLine());
-		
-		//간선 갯수
-		ArrayList<ArrayList<Integer>> verArr = new ArrayList<ArrayList<Integer>>();
-		
-		for(int i=0;i<N+1;i++)verArr.add(new ArrayList<>());
-		
-		StringTokenizer st;
-		for(int i=0;i<N-1;i++) {
-			st = new StringTokenizer(bf.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			verArr.get(a).add(b);
-			verArr.get(b).add(a);
-		}
+    public static void main(String[] args) throws Exception {
 
-		//verArr에 순차대로 있음
-		int[] arr = new int[N+1];
-		Queue<Integer> qu = new ArrayDeque<>();
-		qu.add(1);
-		while(!qu.isEmpty()) {			
-			//큐에서 값 꺼내기 - 해당 값이 부모 값
-			int par = qu.poll();
-			for(int a: verArr.get(par)) {
-				if(arr[a] ==0) {
-					arr[a] = par;
-					qu.add(a);
-				}
-			}
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-		}
+        int N = Integer.parseInt(bf.readLine());
 
-		for(int i=2;i<N+1;i++)sb.append(arr[i]+"\n");
-		System.out.println(sb);
-	}
+        List<List<Integer>> tree = new ArrayList<>();
 
+        for (int i = 0; i < N+1; i++) {
+            tree.add(new ArrayList<>());
+        }
+
+        StringTokenizer st;
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            tree.get(a).add(b);
+            tree.get(b).add(a);
+        }
+
+        int[] parent = new int[N+1];
+
+        ArrayDeque<Integer> qu = new ArrayDeque<>();
+
+        qu.add(1);
+
+        while(!qu.isEmpty()) {
+            int tmp = qu.poll();
+
+            for(int i: tree.get(tmp)) {
+                if(parent[i] == 0) {
+                    parent[i] = tmp;
+                    qu.add(i);
+                }
+            }
+
+
+        }
+
+        for (int i = 2; i < N+1 ; i++) {
+            System.out.println(parent[i]);
+        }
+
+
+
+    }
 }
