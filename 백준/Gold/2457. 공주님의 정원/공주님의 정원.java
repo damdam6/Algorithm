1578ef8b1b7260b120596da12d6161b78e377ec2
이담비 <dambi626626@gmail.com>
2024-10-04 13:51:54 +0900

    [Gold III] Title: 공주님의 정원, Time: 424 ms, Memory: 50492 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -3,76 +3,92 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        Flower[] flowers = new Flower[N];
+    public static void main(String[] args) throws Exception{
 
-        // 월별 누적 일수 배열 (1월부터 12월까지)
-        int[] monthCumDays = new int[13];
-        int[] daysInMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
-        for(int i = 1; i <= 12; i++) {
-            monthCumDays[i] = monthCumDays[i-1] + daysInMonth[i-1];
+        // 날짜 계산기
+        MONTH_DATE = new HashMap<>(){};
+        MONTH_DATE.put(0,0);
+        for (int i = 1; i <= 12; i++) {
+            if(i%2 == 1 && i <= 7){
+                MONTH_DATE.put(i, MONTH_DATE.get(i-1)+ 31);
+            }else if(i%2 == 1){
+                MONTH_DATE.put(i,  MONTH_DATE.get(i-1)+ 30);
+            }else if(i == 2){
+                MONTH_DATE.put(i, MONTH_DATE.get(i-1)+  28);
+            }else if(i >= 8){
+                MONTH_DATE.put(i,  MONTH_DATE.get(i-1)+ 31);
+            }else {
+                MONTH_DATE.put(i, MONTH_DATE.get(i-1)+ 30);
+            }
         }
 
+
+        Comparator<Flower> comp = new Comparator<Flower>() {
+            @Override
+            public int compare(Flower o1, Flower o2) {
+                if(o1.start < o2.start)return -1;
+                if(o1.start > o2.start)return 1;
+                if(o1.end < o2.end)return 1;
+                if(o1.end > o2.end)return -1;
+                return 0;
+            }
+        };
+
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        TreeSet<Flower> flowers = new TreeSet<>(comp);
+
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
+            st = new StringTokenizer(bf.readLine());
             int startM = Integer.parseInt(st.nextToken());
             int startD = Integer.parseInt(st.nextToken());
             int endM = Integer.parseInt(st.nextToken());
             int endD = Integer.parseInt(st.nextToken());
 
-            int start = getNumOfDay(monthCumDays, startM, startD);
-            int end = getNumOfDay(monthCumDays, endM, endD);
-            flowers[i] = new Flower(start, end);
+            Flower f = new Flower(
+                    getNumOfDay(startM, startD),
+                    getNumOfDay(endM, endD)
+            );
+            flowers.add(f);
         }
+        flowerArr = new ArrayList<>(flowers);
 
-        // 꽃들을 시작일 기준으로 오름차순, 시작일이 같다면 끝일 기준으로 내림차순 정렬
-        Arrays.sort(flowers, (a, b) -> {
-            if(a.start != b.start) return Integer.compare(a.start, b.start);
-            return Integer.compare(b.end, a.end);
-        });
-
-        int current = getNumOfDay(monthCumDays, 3, 1); // 3월 1일
-        int targetEnd = getNumOfDay(monthCumDays, 12, 1); // 11월 30일
+        int nowDate = getNumOfDay(3,1);
+        int maxE = nowDate;
         int cnt = 0;
         int idx = 0;
-        int maxEnd = current;
 
-        while(current <= targetEnd) {
-            boolean found = false;
-            // 현재 날짜 이하로 시작하는 꽃들 중 가장 늦게 끝나는 꽃 찾기
-            while(idx < N && flowers[idx].start <= current) {
-                if(flowers[idx].end > maxEnd){
-                    maxEnd = flowers[idx].end;
-                    found = true;
+        int checkEnd = 0;
+        while(nowDate < getNumOfDay(12,1)){
+
+            checkEnd = 0;
+            while(idx < flowerArr.size() && flowerArr.get(idx).start <= nowDate){
+                if(flowerArr.get(idx).end > maxE){
+                    maxE = flowerArr.get(idx).end;
+                    checkEnd = 1;
                 }
                 idx++;
             }
-
-            if(!found){
+            if(checkEnd != 1){
                 cnt = 0;
                 break;
             }
 
             cnt++;
-            current = maxEnd;
-
-            if(current >= targetEnd){
-                break;
-            }
+            nowDate = maxE;
         }
 
-        if(maxEnd < targetEnd){
-            System.out.println(0);
-        } else {
-            System.out.println(cnt);
-        }
+        System.out.println(cnt);
+
     }
 
-    static int getNumOfDay(int[] monthCumDays, int month, int day){
-        return monthCumDays[month-1] + day;
-    }
+    static  ArrayList<Flower> flowerArr;
+    static HashMap<Integer, Integer> MONTH_DATE;
+    static int getNumOfDay(int month, int date){
+        return MONTH_DATE.get(month-1) + date;
+    };
 
     static class Flower{
         int start;
@@ -81,6 +97,14 @@ public class Main {
         public Flower(int start, int end) {
             this.start = start;
             this.end = end;
+        }
+
+        @Override
+        public String toString() {
+            return "Flower{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    '}';
         }
     }
 }
