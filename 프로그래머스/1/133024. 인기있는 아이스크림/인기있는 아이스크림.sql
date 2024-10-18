f3e16458796802d13a6b8ceaf35bfa3f79d6dc6d
이담비 <dambi626626@gmail.com>
2024-10-18 15:21:31 +0900

    [level 1] Title: 인기있는 아이스크림, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+-- 상반기에 판매된 아이스크림의 맛
+-- 총주문량을 기준으로 내림차순 정렬
+-- 총주문량이 같다면 출하 번호를 기준으로 오름차순 정렬하여 조회하는 SQL 문
+
+SELECT FLAVOR_SUM.FLAVOR FROM (SELECT FLAVOR, SHIPMENT_ID, SUM(TOTAL_ORDER) AS SUM_C FROM FIRST_HALF GROUP BY FLAVOR) 
+AS FLAVOR_SUM
+ORDER BY FLAVOR_SUM.SUM_C DESC, FLAVOR_SUM.SHIPMENT_ID ASC;
