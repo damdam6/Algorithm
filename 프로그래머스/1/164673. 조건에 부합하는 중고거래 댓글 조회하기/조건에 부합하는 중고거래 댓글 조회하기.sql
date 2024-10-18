70a724dcbafd3e78bf29bcf8cf2798cf15282a44
이담비 <dambi626626@gmail.com>
2024-10-18 14:35:15 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,11 @@
+-- 2022년 10월에 작성
+-- 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일
+--  댓글 작성일을 기준으로 오름차순 정렬
+-- 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬
+SELECT
+B.TITLE, B.BOARD_ID,
+R.REPLY_ID, R.WRITER_ID, R.CONTENTS,
+DATE_FORMAT(R.CREATED_DATE,'%Y-%m-%d') AS CREATED_DATE
+FROM (SELECT * FROM USED_GOODS_BOARD WHERE MONTH(CREATED_DATE) = 10) B
+JOIN USED_GOODS_REPLY R ON B.BOARD_ID = R.BOARD_ID
+ORDER BY R.CREATED_DATE ASC, B.TITLE ASC;
